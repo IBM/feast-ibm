8e55b3d530af190086e03b76d4b5827d6baf76e7
Abhay Ratnaparkhi <Abhay.Ratnaparkhi1@ibm.com>
2022-11-02 18:59:22 -0500

    fix timestamp issue

--- ibm_data_engine/data_engine_offline_store.py
+++ ibm_data_engine/data_engine_offline_store.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Abhay Ratnaparkhi, Michal Siedlaczek
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Implementation of the offline store backed by the IBM Cloud Data Engine."""
 import json
 import tempfile
@@ -409,7 +423,7 @@ class DataEngineOfflineStore(OfflineStore):
             where_clause = _where(join_key_columns, aliases=("de_a", "de_b"))
             sql_query = (
                 f"SELECT {fields} FROM {data_source.get_table_query_string()} as de_a "
-                f"JOIN ({inner()}) as de_b WHERE {where_clause} AND de_a.timestamp = de_b.timestamp"
+                f"JOIN ({inner()}) as de_b WHERE {where_clause} AND de_a.{timestamp_field} = de_b.timestamp"
             )
             return _sql_builder(config).run_sql(sql_query)
 
