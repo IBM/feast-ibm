2376f08b4e9cd9173ab9e8d22d21d12e47f67d18
Michal Siedlaczek <michal.siedlaczek@ibm.com>
2023-10-10 09:45:07 -0400

    Upgrade Feast to 0.34
    Signed-off-by: Michal Siedlaczek <michal.siedlaczek@ibm.com>

--- ibm_data_engine/__init__.py
+++ ibm_data_engine/__init__.py
@@ -14,7 +14,7 @@
 
 """Implementation of the offline store backed by the IBM Cloud Data Engine."""
 
-__version__ = "0.25.0"
+__version__ = "0.34.1"
 
 from ibm_data_engine.data_engine_offline_store import (
     DataEngineDataSource,
